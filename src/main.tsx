68abf2c292eadc801b476b7b247927fdf56b6bc7
mkfprod <mkfprod2025@gmail.com>
2026-02-08 12:08:11 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    inspectAttr(), 
+    react(),
+    {
+      name: 'add-nojekyll',
+      writeBundle() {
+        const fs = require('fs');
+        const path = require('path');
+        fs.writeFileSync(path.resolve(__dirname, 'dist/.nojekyll'), '');
+      }
+    }
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
