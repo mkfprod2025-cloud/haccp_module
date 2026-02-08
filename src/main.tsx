f621fb68ee47597e3764ea96a11d885ca8d3a18d
mkfprod <mkfprod2025@gmail.com>
2026-02-08 16:44:40 +0100

    Normalize theme provider import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from 'next-themes';
 
 import App from './App';
 import './index.css';
